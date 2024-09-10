93b109cba229906eb1721de571d702edab0d345c
dl364823 <146682379+dl364823@users.noreply.github.com>
2024-09-10 00:03:54 -0400

    def func to generate 10 product names that represent the description and seedwords

--- gpt_dalle_demo_fall24/server.py
+++ gpt_dalle_demo_fall24/server.py
@@ -26,10 +26,10 @@ from pathlib import Path
 
 
 #Thid is the basic data representation
-headline_data = {
-  "headline":"",
-  "summary":"",
-  "keywords":[],
+description_data = {
+  "description":"",
+  "seedwords":"",
+  "names":[],
   "generations":[]
 }
 
@@ -63,30 +63,28 @@ headline_data = {
 
 
 
-sample_headline_data_1 = {
-
-    "headline": "See a new comet before it vanishes for 400 years",
-    "summary": "CAPE CANAVERAL, Fla. (AP) — A newly discovered comet is swinging through our cosmic neighborhood for the first time in more than 400 years. Stargazers across the Northern Hemisphere should catch a glimpse as soon as possible — either this week or early next — because it will be another 400 years before the wandering ice ball returns. The comet, which is kilometer-sized (1/2-mile), will sweep safely past Earth on Sept. 12, passing within 78 million miles (125 million kilometers).",
-    "keywords": [],
+sample_description_data_1 = {
+    "description": "A home milkshake maker",
+    "seedwords": "fast, healthy, compact.",
+    "names": [],
     "generations": [],
 }
 
 
-
 #### INIT with example data
-headline_data = sample_headline_data_1
+description_data = sample_description_data_1
 
 
 
 
 
-@app.route('/submit_headline', methods=['GET', 'POST'])
-def submit_headline():
-    global headline_data
+@app.route('/submit_description', methods=['GET', 'POST'])
+def submit_description():
+    global description_data
     data = request.get_json()   
 
-    headline_data["headline"] = data["headline"]
-    headline_data["summary"] = data["summary"]
+    description_data["description"] = data["description"]
+    description_data["seedwords"] = data["seedwords"]
 
     #send back the WHOLE array of data, so the client can redisplay it
     return jsonify(headline_data)
@@ -128,39 +126,39 @@ def generate_images(prompt):
 
 
 
-@app.route('/get_keywords', methods=['GET', 'POST'])
-def get_keywords():
-    global headline_data
+@app.route('/get_names', methods=['GET', 'POST'])
+def get_names():
+    global description_data
 
-    headline = headline_data["headline"]
-    summary = headline_data["summary"]
+    description = description_data["description"]
+    seedwords = headline_data["seedwords"]
 
-    keywords = get_keywords_for_headline(headline, summary)
-    headline_data["keywords"] = keywords 
+    names = get_keywords_for_product(description, seedwords)
+    description_data["names"] = names
 
     #send back the WHOLE array of data, so the client can redisplay it
-    return jsonify(headline_data)
+    return jsonify(description_data)
 
 
-def parse_keywords_from_gpt_response(keyword_response):    
-    keyword_list = keyword_response.splitlines()
-    new_keyword_list = []
+def parse_names_from_gpt_response(name_response):
+    name_list = name_response.splitlines()
+    new_name_list = []
     for i, item in enumerate(keyword_list):
         item = item.strip()
         if item != "":
             item = item[item.index(".") + 1:]
             item = item.strip()
-            new_keyword_list.append(item)
-    return new_keyword_list
+            new_name_list.append(item)
+    return new_name_list
 
 
-def get_keywords_for_headline(headline, summary):
-    prompt = "Give me 10 keywords that represent this news headline and summary and format it like this: \n 1. keyword1 \n 2. keyword2 \n 3. keyword three. News headline: "+headline+". News summary: "+summary+"."
+def get_names_for_product(description, seedwords):
+    prompt = "Give me 10 product names that represent the description and seedwords. and format it like this: \n 1. name1 \n 2.name2 \n 3. name three. Product description: "+description+". Product seedwords: "+seedwords+"."
     print(prompt)
 
 
     response_raw = client.chat.completions.create(
-      model="gpt-3.5-turbo",
+      model="gpt-4o-mini",
       messages=[
         {
           "role": "user",
@@ -178,20 +176,20 @@ def get_keywords_for_headline(headline, summary):
 
     print(response)
 
-    keyword_list = []
+    name_list = []
     try:
-        keyword_list = parse_keywords_from_gpt_response(response)
+        name_list = parse_names_from_gpt_response(response)
     except:        
-        print("ERROR: gpt keyword response won't parse")
+        print("ERROR: gpt name response won't parse")
         print(response)
 
-    return keyword_list
+    return name_list
 
 
 @app.route('/')
 def home():
     # you can pass in an existing article or a blank one.
-    return render_template('home.html', data=headline_data)   
+    return render_template('home.html', data=description_data)
 
 
 if __name__ == '__main__':
